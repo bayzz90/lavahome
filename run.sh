936cee45712b02d8b7eeef73b2f384a6db35904b
Bayu Yuni Hartanto <bayzz90@gmail.com>
2024-03-22 10:00:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-53dce253-d44d-4776-9601-b2668d68f3ae/)
 
 
 echo "$curl_output" >> Update.tsx
